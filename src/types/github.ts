a2070292552fab8b6c1bc4456710515b7f0769a5
offside <ofsaytadusenpvcboru@gmail.com>
2026-03-18 23:51:51 +0300

    İlk sürüm hazır

---
+++ src/types/github.ts
@@ -0,0 +1,13 @@
+export interface GitHubRepo {
+  name: string;
+  full_name: string;
+  description: string;
+  html_url: string;
+  stargazers_count: number;
+  forks_count: number;
+  language: string | null;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
